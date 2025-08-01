2857463584f3c42f04eb309335deaeaea158ce38
donnyva1991-collab <donnyva1991@gmail.com>
2025-08-02 00:35:25 +0600

    Update Product.js

--- AIGA-academy-application-main/AIGA-academy-website-main/migrator/seeds/Product.js
+++ AIGA-academy-application-main/AIGA-academy-website-main/migrator/seeds/Product.js
@@ -5,18 +5,27 @@ module.exports = {
     try {
       await Product.bulkCreate([
         {
-          image: 'https://example.com/images/product123.jpg',
+          image: 'https://example.com/images/product1.jpg',
           name: 'Running Shoes',
           cost: 89.99,
           isNew: true,
           deliveryCost: 5.99,
           deliveryCity: 'Almaty',
-          desc: 'Lightweight running shoes for everyday training.'
+          desc: 'Lightweight shoes for running.'
+        },
+        {
+          image: 'https://example.com/images/product2.jpg',
+          name: 'Jiu-Jitsu Mat',
+          cost: 29.99,
+          isNew: false,
+          deliveryCost: 3.99,
+          deliveryCity: 'Astana',
+          desc: 'Comfortable mat for fighting sessions.'
         }
       ]);
       console.log('Products seeded');
     } catch (error) {
-      console.error('Product seeder error:', error);
+      console.error('Products seeder error:', error);
       throw error;
     }
   }
